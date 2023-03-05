00c6bfbb090f7618cd37e142a9bacd344a008c6c
anjan <anjan0151@gmail.com>
2023-03-05 12:37:12 +0600

    feat(model): MVC design pattern and security services packages included. Necessary models for user authentication is included

---
+++ src/main/java/com/springboot/rest/authentication/model/Role.java
@@ -0,0 +1,27 @@
+package com.springboot.rest.authentication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+}
---
+++ src/main/java/com/springboot/rest/authentication/model/User.java
@@ -0,0 +1,57 @@
+package com.springboot.rest.authentication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+@Data
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotBlank
+        @Size(max = 20)
+        private String username;
+
+        @NotBlank
+        @Size(max = 50)
+        @Email
+        private String email;
+
+        @NotBlank
+        @Size(max = 120)
+        private String password;
+
+        /*  We use @ManyToMany annotation to create a many-to-many relationship
+  between two entities. In a bi-directional association,
+  the @ManyToMany annotation is used on both the entities but
+  only one entity can be the owner of the relationship.*/
+
+
+/*  The entity that specifies the @JoinTable is the owning side of the
+  relationship and the entity that specifies the mappedBy attribute
+  is the inverse side.*/
+
+        @ManyToMany(fetch = FetchType.LAZY)
+        @JoinTable(  name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+        private Collection<Role> roles;
+
+}
