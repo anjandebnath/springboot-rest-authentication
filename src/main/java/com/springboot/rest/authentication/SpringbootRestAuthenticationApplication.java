341bdccb16c90badadbfdc89b6191e8125f5eaf1
Anjan Debnath <ad@ixora.com>
2023-03-05 11:38:19 +0600

    feat(kick_start): Initialize spring boot project from https://start.spring.io/

---
+++ src/main/java/com/springboot/rest/authentication/SpringbootRestAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.rest.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/rest/authentication/SpringbootRestAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.rest.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
