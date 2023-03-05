8714dd0f18c14400d08fb7ea7c9d66db05efcb46
anjan <anjan0151@gmail.com>
2023-03-05 14:21:47 +0600

    feat(data preload): user data preload

--- src/main/java/com/springboot/rest/authentication/SpringbootRestAuthenticationApplication.java
+++ src/main/java/com/springboot/rest/authentication/SpringbootRestAuthenticationApplication.java
@@ -1,13 +1,27 @@
 package com.springboot.rest.authentication;
 
+import com.springboot.rest.authentication.model.ERole;
+import com.springboot.rest.authentication.model.Role;
+import com.springboot.rest.authentication.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootRestAuthenticationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestAuthenticationApplication.class, args);
+	}
+
+	/*Preload the data to insert into database*/
+	@Bean
+	public CommandLineRunner roleLoader(RoleRepository repo) {
+		return args -> {
+			repo.save(new Role(ERole.ROLE_ADMIN));
+			repo.save(new Role(ERole.ROLE_USER));
+		};
 	}
 
 }
--- src/main/java/com/springboot/rest/authentication/model/Role.java
+++ src/main/java/com/springboot/rest/authentication/model/Role.java
@@ -25,4 +25,8 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private Collection<User> users;
+
+    public Role(ERole role){
+
+    }
 }
